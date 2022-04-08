c4b6450a5254c9f576b3f847f517f942b201d010
FinnS013 <92523249+FinnS013@users.noreply.github.com>
2022-04-08 15:02:45 +0000

    variabellen (const) voor de toetsen W,A,S,D gemaakt voor het bewegen van de character

--- script.js
+++ script.js
@@ -14,6 +14,11 @@ const SPELEN = 1;
 const GAMEOVER = 2;
 var spelStatus = SPELEN;
 
+const KEY_W = 87;
+const KEY_A = 65;
+const KEY_S = 83;
+const KEY_D = 68;
+
 var spelerX = 600; // x-positie van speler
 var spelerY = 600; // y-positie van speler
 
