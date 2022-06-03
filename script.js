6ef230348801b3ba9a1036c0413f65267db2c3d2
FinnS013 <92523249+FinnS013@users.noreply.github.com>
2022-06-03 19:56:51 +0000

    code voor uiterlijk speler in functie gezet

--- script.js
+++ script.js
@@ -117,6 +117,74 @@ var vijand = function() {
 }
 
 
+var speler = function() {
+  //torso
+  fill(181, 9, 9);
+  rect(spelerX, spelerY, 30, 50); //vest of torso
+  
+  //riem
+  fill(255, 255, 255);
+  rect(spelerX, spelerY+35, 30, 5 );
+  fill(201, 168, 0);
+  rect(spelerX+13, spelerY+35, 5, 5); //gesp
+  
+  //benen
+  //linker been
+  fill(12, 15, 138);
+  rect(spelerX, spelerY+50, 10, 50); //broek
+  fill(0, 0, 0);
+  rect(spelerX, spelerY+90, 10, 10); //schoen
+  
+  //rechter been
+  fill(12, 15, 138);
+  rect(spelerX+20, spelerY+50, 10, 50); //broek
+  fill(0, 0, 0);
+  rect(spelerX+20, spelerY+90, 10, 10); //schoen
+  
+  //rechter arm (schouder achter geweer)
+  fill(181, 9, 9);
+  rect(spelerX+30, spelerY, 10, 20); //mouw boven stuk (rood)
+  
+  //geweer
+  fill(117, 54, 54);
+  rect(spelerX, spelerY+10, 85, 8); //houten stuk
+  
+  fill(105, 101, 101);
+  rect(spelerX+25, spelerY+8, 75, 5); //loop
+  
+  fill(148, 138, 138);
+  rect(spelerX+95, spelerY+5, 30, 3);
+  
+  //arm
+  //linker arm (van user perspecftief)
+  fill(181, 9, 9);
+  rect(spelerX-10, spelerY, 10, 25); //mouw boven stuk (rood)
+  
+  fill(181, 9, 9);
+  rect(spelerX-10, spelerY+15, 40, 10); //mouw boven stuk onderarm(rood)
+  
+  fill(12, 15, 138);
+  rect(spelerX+20, spelerY+15, 10, 10); //mouw onderstuk (blauw)
+  
+  fill(230, 200, 185);
+  rect(spelerX+30, spelerY+15, 10, 10); //hand
+  
+  //hoofd
+  fill(230, 200, 185);
+  rect(spelerX +5, spelerY-20, 20, 20); //head
+  
+  //kraag blauw
+  fill(12, 15, 138);
+  rect(spelerX+4, spelerY-2, 22, 5);
+  
+  //hoed
+  fill(0, 0, 0);
+  rect(spelerX+3, spelerY-50, 25, 30); //hoed
+  fill(192, 192, 192);
+  rect(spelerX+10, spelerY-27, 10, 7);// plaat op hoed
+}
+
+
 /**
  * Updatet globale variabelen met posities van speler, vijanden en kogels
  */
@@ -243,70 +311,7 @@ var tekenAlles = function () {
   ellipse(kogelX, kogelY, 20, 20);
   
   // speler
-  //torso
-  fill(181, 9, 9);
-  rect(spelerX, spelerY, 30, 50); //vest of torso
-  
-  //riem
-  fill(255, 255, 255);
-  rect(spelerX, spelerY+35, 30, 5 );
-  fill(201, 168, 0);
-  rect(spelerX+13, spelerY+35, 5, 5); //gesp
-  
-  //benen
-  //linker been
-  fill(12, 15, 138);
-  rect(spelerX, spelerY+50, 10, 50); //broek
-  fill(0, 0, 0);
-  rect(spelerX, spelerY+90, 10, 10); //schoen
-  
-  //rechter been
-  fill(12, 15, 138);
-  rect(spelerX+20, spelerY+50, 10, 50); //broek
-  fill(0, 0, 0);
-  rect(spelerX+20, spelerY+90, 10, 10); //schoen
-  
-  //rechter arm (schouder achter geweer)
-  fill(181, 9, 9);
-  rect(spelerX+30, spelerY, 10, 20); //mouw boven stuk (rood)
-  
-  //geweer
-  fill(117, 54, 54);
-  rect(spelerX, spelerY+10, 85, 8); //houten stuk
-  
-  fill(105, 101, 101);
-  rect(spelerX+25, spelerY+8, 75, 5); //loop
-  
-  fill(148, 138, 138);
-  rect(spelerX+95, spelerY+5, 30, 3);
-  
-  //arm
-  //linker arm (van user perspecftief)
-  fill(181, 9, 9);
-  rect(spelerX-10, spelerY, 10, 25); //mouw boven stuk (rood)
-  
-  fill(181, 9, 9);
-  rect(spelerX-10, spelerY+15, 40, 10); //mouw boven stuk onderarm(rood)
-  
-  fill(12, 15, 138);
-  rect(spelerX+20, spelerY+15, 10, 10); //mouw onderstuk (blauw)
-  
-  fill(230, 200, 185);
-  rect(spelerX+30, spelerY+15, 10, 10); //hand
-  
-  //hoofd
-  fill(230, 200, 185);
-  rect(spelerX +5, spelerY-20, 20, 20); //head
-  
-  //kraag blauw
-  fill(12, 15, 138);
-  rect(spelerX+4, spelerY-2, 22, 5);
-  
-  //hoed
-  fill(0, 0, 0);
-  rect(spelerX+3, spelerY-50, 25, 30); //hoed
-  fill(192, 192, 192);
-  rect(spelerX+10, spelerY-27, 10, 7);// plaat op hoed
+  speler();
 
   // punten en health
   textSize(25);
